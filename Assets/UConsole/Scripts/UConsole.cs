34a652a7c161f2db73bd0dcd29ef86e31e8cc4a3
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2019-08-08 18:55:33 +0100

    Removed Namespace

--- Assets/UConsole/Scripts/UConsole.cs
+++ Assets/UConsole/Scripts/UConsole.cs
@@ -6,181 +6,179 @@ using System;
 using System.Text.RegularExpressions;
 using System.Linq;
 
-namespace UConsole
+
+public class UConsole : MonoBehaviour
 {
-    public class UConsole : MonoBehaviour
+    public static UConsole instance { private set; get; }
+    [Header("Settings")]
+    public KeyCode ToggleConsoleKey = KeyCode.F1;
+    public bool isEnabled { get; private set; } = false;
+    public List<UCommand> commands { private set; get; } = new List<UCommand>();
+    [Tooltip("Shows the type of message it was, log, warning or error")]
+    public bool showType = false;
+
+    [SerializeField]
+    private InputField input;
+    [SerializeField]
+    private Text output;
+    [SerializeField]
+    private GameObject consoleHolder;
+    [SerializeField]
+    private bool DoNotDestroyOnLoad = true;
+
+    private List<string> lines = new List<string>();
+    private string[] visableLines;
+    private int startLine = 0;
+
+    private void Awake()
     {
-        public static UConsole instance { private set; get; }
-        [Header("Settings")]
-        public KeyCode ToggleConsoleKey = KeyCode.F1;
-        public bool isEnabled { get; private set; } = false;
-        public List<UCommand> commands { private set; get; } = new List<UCommand>();
-        [Tooltip("Shows the type of message it was, log, warning or error")]
-        public bool showType = false;
+        if (UConsole.instance == null)
+            UConsole.instance = this;
+        else
+            Destroy(this.gameObject);
 
-        [SerializeField]
-        private InputField input;
-        [SerializeField]
-        private Text output;
-        [SerializeField]
-        private GameObject consoleHolder;
-        [SerializeField]
-        private bool DoNotDestroyOnLoad = true;
+        if (DoNotDestroyOnLoad)
+            DontDestroyOnLoad(this.gameObject);
+    }
+    private void Start()
+    {
+        CreateDefaultCommands();
+        Application.logMessageReceived += LogMessageReceived;
+        consoleHolder.SetActive(isEnabled);
+    }
 
-        private List<string> lines = new List<string>();
-        private string[] visableLines;
-        private int startLine = 0;
+    private void LogMessageReceived(string condition, string stackTrace, LogType type)
+    {
+        lines.Add((showType ? type.ToString() + ": " : "") + condition);
+    }
+    private void CreateDefaultCommands()
+    {
+        UCommand helpcommand = new UCommand("help", "help <command>");
+        helpcommand.callbacks.Add(Help);
+        AddCommand(helpcommand);
 
-        private void Awake()
+        UCommand clearcommand = new UCommand("clear", "clear");
+        clearcommand.callbacks.Add(Clear);
+        AddCommand(clearcommand);
+    }
+    private void Clear(string[] obj)
+    {
+        lines = new List<string>();
+        startLine = 0;
+        UpdateVisableLines();
+    }
+    private void Help(string[] obj)
+    {
+        if (obj.Length == 0)
         {
-            if (UConsole.instance == null)
-                UConsole.instance = this;
-            else
-                Destroy(this.gameObject);
-
-            if (DoNotDestroyOnLoad)
-                DontDestroyOnLoad(this.gameObject);
-        }
-        private void Start()
-        {
-            CreateDefaultCommands();
-            Application.logMessageReceived += LogMessageReceived;
-            consoleHolder.SetActive(isEnabled);
-        }
-
-        private void LogMessageReceived(string condition, string stackTrace, LogType type)
-        {
-            lines.Add((showType? type.ToString() + ": " : "") + condition);
-        }
-        private void CreateDefaultCommands()
-        {
-            UCommand helpcommand = new UCommand("help", "help <command>");
-            helpcommand.callbacks.Add(Help);
-            AddCommand(helpcommand);
-
-            UCommand clearcommand = new UCommand("clear", "clear");
-            clearcommand.callbacks.Add(Clear);
-            AddCommand(clearcommand);
-        }
-        private void Clear(string[] obj)
-        {
-            lines = new List<string>();
-            startLine = 0;
-            UpdateVisableLines();
-        }
-        private void Help(string[] obj)
-        {
-            if (obj.Length == 0)
-            {
-                Debug.Log("All Commands");
-                for (int i = 0; i < commands.Count; i++)
-                {
-                    Debug.Log(commands[i].command + " \"" + commands[i].usage + "\"");
-                }
-            }
-            else
-            {
-                for (int i = 0; i < commands.Count; i++)
-                {
-                    if (commands[i].command.ToLower() == obj[0].ToLower())
-                    {
-                        Debug.Log("Command : " + commands[i].command + " \"" + commands[i].usage + "\"");
-                        break;
-                    }
-                    
-                }
-            }
-                
-        }
-        private void Update()
-        {
-            if (Input.GetKeyDown(ToggleConsoleKey))
-            {
-                consoleHolder.SetActive(!isEnabled);
-                isEnabled = !isEnabled;
-            }
-
-            if (isEnabled)
-                IsEnabled();
-        }
-        private void IsEnabled()
-        {
-            if (input.text != string.Empty && Input.GetKeyDown(KeyCode.Return))
-            {
-                RunCommand(input.text);
-                input.text = string.Empty;
-                startLine = lines.Count - (lines.Count > 9 ? 9 : lines.Count);
-                UpdateVisableLines();
-                input.Select();
-            }
-
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                startLine--;
-                UpdateVisableLines();
-            }
-            else if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                startLine++;
-                UpdateVisableLines();
-            }
-        }
-        private void UpdateVisableLines()
-        {
-            startLine = Mathf.Clamp(startLine, 0, lines.Count - (lines.Count > 9 ? 9 : lines.Count));
-            visableLines = new string[(lines.Count > 9 ? 9 : lines.Count)];
-            Array.Copy(lines.ToArray(), startLine, visableLines, 0, lines.Count >= 9 ? 9 : lines.Count);
-            output.text = string.Join("\n", visableLines);
-        }
-        public void RunCommand(string command)
-        {
-            Debug.Log(command);
-            string[] args = command.Split(' ');
+            Debug.Log("All Commands");
             for (int i = 0; i < commands.Count; i++)
             {
-                if (commands[i].command.ToLower() == args[0].ToLower())
-                {
-                    string[] shortargs = new string[args.Length - 1];
-                    Array.Copy(args, 1, shortargs, 0, args.Length - 1);
-                    for (int j = 0; j < commands[i].callbacks.Count; j++)
-                    {
-                        commands[i].callbacks[j].Invoke(shortargs);
-                    }
-                    return;
-                }
+                Debug.Log(commands[i].command + " \"" + commands[i].usage + "\"");
             }
-            Debug.LogError("Unknow command " + command + ". Try \"help\" to see all the commands");
         }
-       
-        public void AddCommand(UCommand newCommand)
+        else
         {
-            commands.Add(newCommand);
-        }
-        public bool RemoveCommand(string command)
-        {
-            for (int i = 0; i < command.Length; i++)
+            for (int i = 0; i < commands.Count; i++)
             {
-                if (commands[i].command == command)
+                if (commands[i].command.ToLower() == obj[0].ToLower())
                 {
-                    commands.RemoveAt(i);
-                    return true;
+                    Debug.Log("Command : " + commands[i].command + " \"" + commands[i].usage + "\"");
+                    break;
                 }
+
             }
-            return false;
         }
-    }
 
-    public class UCommand
+    }
+    private void Update()
     {
-        public string command { private set; get; }
-        public string usage { private set; get; }
-        public List<Action<string[]>> callbacks;
-        public UCommand (string command, string usage)
+        if (Input.GetKeyDown(ToggleConsoleKey))
         {
-            this.command = command;
-            this.usage = usage;
-            callbacks = new List<Action<string[]>>();
+            consoleHolder.SetActive(!isEnabled);
+            isEnabled = !isEnabled;
+        }
+
+        if (isEnabled)
+            IsEnabled();
+    }
+    private void IsEnabled()
+    {
+        if (input.text != string.Empty && Input.GetKeyDown(KeyCode.Return))
+        {
+            RunCommand(input.text);
+            input.text = string.Empty;
+            startLine = lines.Count - (lines.Count > 9 ? 9 : lines.Count);
+            UpdateVisableLines();
+            input.Select();
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            startLine--;
+            UpdateVisableLines();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            startLine++;
+            UpdateVisableLines();
         }
     }
+    private void UpdateVisableLines()
+    {
+        startLine = Mathf.Clamp(startLine, 0, lines.Count - (lines.Count > 9 ? 9 : lines.Count));
+        visableLines = new string[(lines.Count > 9 ? 9 : lines.Count)];
+        Array.Copy(lines.ToArray(), startLine, visableLines, 0, lines.Count >= 9 ? 9 : lines.Count);
+        output.text = string.Join("\n", visableLines);
+    }
+    public void RunCommand(string command)
+    {
+        Debug.Log(command);
+        string[] args = command.Split(' ');
+        for (int i = 0; i < commands.Count; i++)
+        {
+            if (commands[i].command.ToLower() == args[0].ToLower())
+            {
+                string[] shortargs = new string[args.Length - 1];
+                Array.Copy(args, 1, shortargs, 0, args.Length - 1);
+                for (int j = 0; j < commands[i].callbacks.Count; j++)
+                {
+                    commands[i].callbacks[j].Invoke(shortargs);
+                }
+                return;
+            }
+        }
+        Debug.LogError("Unknow command " + command + ". Try \"help\" to see all the commands");
+    }
 
+    public void AddCommand(UCommand newCommand)
+    {
+        commands.Add(newCommand);
+    }
+    public bool RemoveCommand(string command)
+    {
+        for (int i = 0; i < command.Length; i++)
+        {
+            if (commands[i].command == command)
+            {
+                commands.RemoveAt(i);
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+public class UCommand
+{
+    public string command { private set; get; }
+    public string usage { private set; get; }
+    public List<Action<string[]>> callbacks;
+    public UCommand(string command, string usage)
+    {
+        this.command = command;
+        this.usage = usage;
+        callbacks = new List<Action<string[]>>();
+    }
+}
+
